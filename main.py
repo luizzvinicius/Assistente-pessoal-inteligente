4e275b9110320b5644ec5135b3d6bdd50e31b76d
Luiz Vinícius <93850693+luizzvinicius@users.noreply.github.com>
2025-07-28 21:45:45 -0300

    feat: format document & simplified main function

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
 import ollama
-import utils
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_ollama import OllamaEmbeddings, ChatOllama
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain_ollama import ChatOllama, OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+import utils
 
 MODEL = "llama3.2"
 CHROMA_DB_DIR = "./db"
@@ -34,18 +34,20 @@ def add_document():
     if utils.is_empty_directory(PATH_ADD_DOC):
         print("\033[93mNothing to add.\033[0m\n")
         return
-        
+
     files = os.listdir(PATH_ADD_DOC)
 
     for file in files:
         if utils.is_document_added(file):
-            print(f"Remove document {file} from base_de_documentos.txt. This file was already added to {MODEL}.\n")
+            print(
+                f"Remove document {file} from base_de_documentos.txt. This file was already added to {MODEL}.\n"
+            )
             return
 
     vector_db = Chroma(
         persist_directory=CHROMA_DB_DIR,
         embedding_function=OllamaEmbeddings(model="nomic-embed-text"),
-        collection_name="simple-rag"
+        collection_name="simple-rag",
     )
 
     for file in files:
@@ -54,10 +56,10 @@ def add_document():
         splitter = RecursiveCharacterTextSplitter(chunk_size=1200, chunk_overlap=300)
         chunks = splitter.split_documents(pages)
         ollama.pull("nomic-embed-text")
-        
+
         vector_db.add_documents(chunks)
         utils.write_document_name(file)
-    
+
     print("arquivos adicionados")
 
 
@@ -65,23 +67,23 @@ def chat_llm():
     if not utils.is_directory(CHROMA_DB_DIR):
         print("\033[93mNo documents added to database.\033[0m\n")
         return
-    
+
     print("Loading documents...")
     vector_db = Chroma(
         embedding_function=OllamaEmbeddings(model="nomic-embed-text"),
         collection_name="simple-rag",
-        persist_directory=CHROMA_DB_DIR
+        persist_directory=CHROMA_DB_DIR,
     )
     ollama.pull("nomic-embed-text")
 
     retriever = MultiQueryRetriever.from_llm(
-    vector_db.as_retriever(), LLM, prompt=QUERY_PROMPT
+        vector_db.as_retriever(), LLM, prompt=QUERY_PROMPT
     )
 
     while True:
         template = """Answer the question based ONLY on the following context: {context}
         Question: {question} """
-        
+
         prompt = ChatPromptTemplate.from_template(template)
         chain = (
             {"context": retriever, "question": RunnablePassthrough()}
@@ -94,20 +96,26 @@ def chat_llm():
         if "-exit" in user_prompt:
             finish_chat()
             return
-                
+
         llm_response = chain.invoke(input=(user_prompt,))
         print(f"\nOllama: {llm_response}\n")
 
 
 def main():
     while True:
-        opt = utils.read_option("1- Conversar com o Assitente\n2- Adicionar documento\n3- Sair\nDigite sua opção: ", 3)
+        opt = utils.read_option(
+            "1- Conversar com o Assistente\n2- Adicionar documento\n3- Sair\nDigite sua opção: ",
+            3,
+        )
         match opt:
-            case 1: chat_llm()
-            case 2: add_document()
-            case 3: break
-            case _: continue
+            case 1:
+                chat_llm()
+            case 2:
+                add_document()
+            case 3:
+                break
     finish_chat()
+
 
 if __name__ == "__main__":
     main()
